eebdbd35346b94f0cdd2fd05871ecf7bb3f10d3f
Jesslyn <jesslyn@sg-edts.com>
2024-12-02 17:48:25 +0700

    Update

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
-package storage
+package main
 
 import (
 	"context"
 	"fmt"
-	_types "v1/types"
-	_gcs "v1/v1/gcs"
-	_minio "v1/v1/minio"
+	"v1/storage/gcs"
+	"v1/storage/minio"
+	_types "v1/storage/types"
 )
 
 // NewStorageClient initializes and returns either a GCS or MinIO client based on the config.
@@ -13,17 +13,17 @@ import (
 func NewStorageClient(ctx context.Context, storageType _types.StorageType, config interface{}) (_types.StorageClient, error) {
 	switch storageType {
 	case _types.StorageTypeGCS:
-		gcsConfig, ok := config.(_gcs.Config)
+		gcsConfig, ok := config.(gcs.Config)
 		if !ok {
 			return nil, fmt.Errorf("invalid config for GCS client")
 		}
-		return _gcs.NewGcsClient(ctx, gcsConfig)
+		return gcs.NewGcsClient(ctx, gcsConfig)
 	case _types.StorageTypeMinio:
-		minioConfig, ok := config.(_minio.Config)
+		minioConfig, ok := config.(minio.Config)
 		if !ok {
 			return nil, fmt.Errorf("invalid config for MinIO client")
 		}
-		return _minio.NewMinioClient(minioConfig)
+		return minio.NewMinioClient(minioConfig)
 	default:
 		return nil, fmt.Errorf("unsupported storage type: %s", storageType)
 	}
